f7e376676070d01ff27b9c6ee1c0db6ce41e76b7
Mariam Ramadan <mariamramadan@aucegypt.edu>
2023-01-16 18:39:23 +0200

    FireBase authentication

--- Linkup/app/src/main/java/com/mariamramadan/link_up/AccountType.java
+++ Linkup/app/src/main/java/com/mariamramadan/link_up/AccountType.java
@@ -3,11 +3,14 @@ package com.mariamramadan.link_up;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountType extends AppCompatActivity {
 
@@ -19,6 +22,25 @@ public class AccountType extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.linkup_background)));
+
+        Button Client = (Button) findViewById(R.id.ClientButton);
+        Button ServiceProvider = (Button) findViewById(R.id.ServiceProviderButton);
+
+        Client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ClientSignup = new Intent(AccountType.this, SignUpClient.class);
+                startActivity(ClientSignup);
+            }
+        });
+        ServiceProvider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ServiceProviderSignup = new Intent(AccountType.this, SignUpService.class);
+                startActivity(ServiceProviderSignup);
+            }
+        });
+
 
     }
 
--- Linkup/app/src/main/java/com/mariamramadan/link_up/SignUpClient.java
+++ Linkup/app/src/main/java/com/mariamramadan/link_up/SignUpClient.java
@@ -1,14 +1,110 @@
 package com.mariamramadan.link_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpClient extends AppCompatActivity {
+
+    EditText email, Password, ConfPassword;
+    Button submit;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mauth;
+    FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_client);
+
+         email = (EditText) findViewById(R.id.EmailClient);
+         Password = (EditText) findViewById(R.id.PassClient);
+        ConfPassword = (EditText) findViewById(R.id.ConfPassClient);
+
+         submit = (Button) findViewById(R.id.SubmitClient);
+         progressDialog = new ProgressDialog(this);
+         mauth = FirebaseAuth.getInstance();
+         mUser = mauth.getCurrentUser();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            PerformAuth();
+
+
+            }
+        });
+
+
+
     }
+
+    private void PerformAuth() {
+        String inputEmail = email.getText().toString();
+        String inputPassword = Password.getText().toString();
+        String ConfirmPassword = ConfPassword.getText().toString();
+
+        if(!inputEmail.matches(emailPattern))
+        {
+            email.setError("Email is wrong");
+        }
+        else if(inputPassword.isEmpty() || inputPassword.length() < 8)
+        {
+            Password.setError("Enter a proper password with at least 8 characters");
+        }
+        else if(!inputPassword.equals(ConfirmPassword))
+        {
+            ConfPassword.setError("Passwords do not match");
+        }
+        else
+        {
+            progressDialog.setMessage("Completing sign up.....");
+            progressDialog.setTitle("Sign up");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+            mauth.createUserWithEmailAndPassword(inputEmail,inputPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        ToClientHome();
+                        Toast.makeText(SignUpClient.this, "Sign up Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignUpClient.this, " "+ task.getException(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    private void ToClientHome() {
+        Intent ClientHome = new Intent(SignUpClient.this, ServicesMenu.class);
+        ClientHome.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(ClientHome);
+
+    }
+
 }
