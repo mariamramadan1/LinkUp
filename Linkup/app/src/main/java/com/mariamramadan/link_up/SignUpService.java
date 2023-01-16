10d4329900f64051ae30b439176c33b0e36cf925
Mariam Ramadan <mariamramadan@aucegypt.edu>
2023-01-16 17:04:14 +0200

    Sign up added

---
+++ Linkup/app/src/main/java/com/mariamramadan/link_up/SignUpService.java
@@ -0,0 +1,14 @@
+package com.mariamramadan.link_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_service);
+    }
+}
