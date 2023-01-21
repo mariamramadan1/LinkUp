89a03a62e05b650c34bafada6836b5e311c3dafa
nourmontasser <64047587+nourmontasser@users.noreply.github.com>
2023-01-21 15:22:42 +0200

    notfication trial

---
+++ Linkup/app/src/main/java/com/mariamramadan/link_up/PushNotificationService.java
@@ -0,0 +1,44 @@
+package com.mariamramadan.link_up;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+import androidx.core.app.NotificationManagerCompat;
+
+public class PushNotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        String title = remoteMessage.getNotification().getTitle();
+        String text = remoteMessage.getNotification().getBody();
+
+        final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Heads Up Notification",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+        }
+
+        Notification.Builder notification =
+                null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this, CHANNEL_ID)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setAutoCancel(true);
+        }
+
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+
+        super.onMessageReceived(remoteMessage);
+    }
+}
